b0989e205ac70f96200a4fbc36156f28c26fc559
Erick Calder <e@arix.com>
2019-04-22 09:01:13 -0700

    - scaffold now includes MongoDb sample code for state implementation

--- scaffold/server.js
+++ scaffold/server.js
@@ -52,13 +52,30 @@ var self = {
 	}
 }
 
-// Now lambda expressions are stateless.  if you need to keep state across
-// calls (as is needed in dialogues), provide a state machine.  Below is a
-// skeleton you can fill in.  Keep in mind that saving data to the filesystem
-// is not a good solution as it, too, is destroyed when the virtual machine is
-// shut down, so MongoDb is your friend
+/*
+Lambda expressions in Now are stateless.  if you need to keep state across
+calls (as is needed in dialogues), you'll need to provide a state machine.
+Below is a skeleton you can fill in.  Keep in mind that saving data to the
+filesystem (typically to /tmp) is not a good solution as it, too, is destroyed
+when the virtual machine is shut down, so MongoDb is your friend.  Get an
+account and install it like this:
+
+$ npm install mongodb --save
+*/
+
+// to use MongoDb, uncomment the code below and edit it with relevant
+// information.  You may delete the current implementation bodies as 
+// they are stubs to make the scaffold work
+
+// const MongoDb = require('mongodb')
+// const dbc = MongoDb.MongoClient.connect(
+// 	'your-connection-uri', { useNewUrlParser: true }
+// );
+// const dbn = 'your-database-name';
+// const cnm = 'a-collection-name';
 
 var state = {
+	// remove the element below if using MongoDb
 	cache: {
 		[bot.info.username]: {
 			'test_username': { 'dialogue': [] },
@@ -68,16 +85,26 @@ var state = {
 		}
 	},
 	get(app, user, k) {
+		// if using MongoDb, remove the following
+		// lines and uncomment the code below it
+
 		var p = ['cache', app, user, k].join('/');
-		return this.getpath(p)
+		return this.getpath(p) || {route: ''}
+
+		// return dbc.db(dbn).collection(cnm)
+		// 	.then(t => t.find({app, user}))
+		// 	.then(ls => ls.toArray())
+		// 	.then(ls => (ls.length > 0 ? ls[0][k] : undefined) || {});
 	},
 	save(app, user, k, o) {
+		// if using MongoDb, remove the following
+		// lines and uncomment the code below it
+
 		var p = ['cache', app, user, k].join('/');
 		this.setpath(p, o)
-	},
-	rm(app, user, k) {
-		var p = ['cache', app, user, k].join('/');
-		this.setpath(p, [])
+
+		// return dbc.db(dbn).collection(cnm)
+		// 	.then(t => t.updateOne({app, user}, o, {upsert: true}))
 	}
 }
 
